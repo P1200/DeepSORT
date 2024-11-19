d8ae0aa979fbe473f985de6da9a94acbdd3e029b
P1200 <sekwoja5@gmail.com>
2024-11-19 10:54:25 +0100

    In progress...

--- System.py
+++ System.py
@@ -1,8 +1,12 @@
+import math
 import time
 
 import cv2
+import numpy as np
 
+from BoundingBox import BoundingBox
 from Detector import Detector
+from Tracker import Tracker
 from VideoPlayer import VideoPlayer
 
 
@@ -25,11 +29,48 @@ class System:
 
         print("Preparing detections...")
         start_time = time.time()
+        trackers = []
+
         while ret:
-            new_frame = detector.detectYOLOv4(frame)
-            new_capture.append(new_frame)
+            row = []
+            boxes = detector.detectYOLOv4(frame)
+            for i in range(len(boxes)):
+                x, y, w, h, center_x, center_y = boxes[i]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                if len(trackers) > 0:
+                    column = []
+                    for j in range(len(trackers)):
+                        column.append(self.count_distance(boxes[i], trackers[j]))
+                    row.append(column)
+
+                    value = max(column)
+
+                    if value > 500:
+                        tracker = Tracker(center_x, center_y)
+                        trackers.append(tracker)
+                    else:
+                        tracker_id = column.index(value)
+                        column.insert(tracker_id, None)
+                        tracker = trackers[tracker_id]
+                else:
+                    tracker = Tracker(center_x, center_y)
+                    trackers.append(tracker)
+                pred_x, pred_y = tracker.predict()
+                tracker.update(center_x, center_y)
+
+                print(len(trackers))
+                cv2.circle(frame, (pred_x, pred_y), 5, (0, 0, 255), -1)
+
+            new_capture.append(frame)
             ret, frame = capture.read()
 
         print("Detections are ready in %s." % (time.time() - start_time))
         capture.release()
         video_player.play(new_capture)
+
+    def count_distance(self, box: [BoundingBox], tracker: [Tracker]) -> float:
+        pred_x, pred_y = tracker.predict()
+        distance_x = box.center_x - pred_x
+        distance_y = box.center_y - pred_y
+        return math.sqrt(distance_x ** 2 + distance_y ** 2)
---
+++ Tracker.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+
+class Tracker:
+    def __init__(self, init_x: [int], init_y: [int]):
+        # Ustawienia dla filtru Kalmana
+        self.kalman = cv2.KalmanFilter(4, 2)  # 4 stany (x, y, dx, dy), 2 obserwacje (x, y)
+        self.kalman.measurementMatrix = np.array([[1, 0, 0, 0],
+                                             [0, 1, 0, 0]], np.float32)
+        self.kalman.transitionMatrix = np.array([[1, 0, 1, 0],
+                                            [0, 1, 0, 1],
+                                            [0, 0, 1, 0],
+                                            [0, 0, 0, 1]], np.float32)
+        self.kalman.processNoiseCov = np.eye(4, dtype=np.float32) * 0.03  # Szum procesu
+        self.kalman.measurementNoiseCov = np.eye(2, dtype=np.float32) * 1  # Szum pomiaru
+
+        measurement = np.array([[np.float32(init_x)],
+                                [np.float32(init_y)]])
+        self.kalman.correct(measurement)  # Korekcja na podstawie pomiaru
+
+    def predict(self):
+
+        prediction = self.kalman.predict()
+        pred_x, pred_y = int(prediction[0]), int(prediction[1])
+        return pred_x, pred_y
+
+    def update(self, center_x, center_y):
+        measurement = np.array([[np.float32(center_x)],
+                                [np.float32(center_y)]])
+        self.kalman.correct(measurement)  # Korekcja na podstawie pomiaru
