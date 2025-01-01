ff987a2f7f2a37a81bc98efae6c170d1057dcdb3
P1200 <sekwoja5@gmail.com>
2025-01-01 01:04:31 +0100

    Add hungarian algorithm.

--- BoundingBox.py
+++ BoundingBox.py
@@ -1,12 +1,13 @@
 class BoundingBox:
-    def __init__(self, x: [int], y: [int], w: [int], h: [int], center_x: [int], center_y: [int]):
+    def __init__(self, x: [int], y: [int], w: [int], h: [int]):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
-        self.center_x = center_x
-        self.center_y = center_y
+        # self.center_x = center_x
+        # self.center_y = center_y
         self.is_used = False
+        self.descriptor = None
 
     def __iter__(self):
-        return iter((self.x, self.y, self.w, self.h, self.center_x, self.center_y))
+        return iter((self.x, self.y, self.w, self.h))
